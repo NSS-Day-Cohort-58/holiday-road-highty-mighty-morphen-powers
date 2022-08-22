4ec5da2568c016f7cdd3a43df12a9bec3b05be04
Nora Szeto <neszeto@gmail.com>
2022-08-22 11:55:49 -0500

    "merge conflicts"

--- scripts/dataAccess.js
+++ scripts/dataAccess.js
@@ -8,15 +8,15 @@ export const setParks = (parks) => {
     applicationState.parks = parks
 }
 
-<<<<<<< HEAD
+
 
 
 
 
 export const setAttractions = (attractions) => {
     applicationState.attractions = attractions
-=======
+}
+
 export const setEateries = (eateries) => {
     applicationState.eateries = eateries
->>>>>>> main
 }
