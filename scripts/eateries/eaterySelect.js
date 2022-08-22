53de295d3bac5763fe427dc243db596fa2530db2
Adam Chance <105458461+AChan118@users.noreply.github.com>
2022-08-22 15:13:29 -0500

    Update eaterySelect.js

--- scripts/eateries/eaterySelect.js
+++ scripts/eateries/eaterySelect.js
@@ -2,7 +2,7 @@ import { setEatery } from "../dataAccess.js"
 
 export const eaterySelect = (eateries) => {
     return `<select id="eaterySelect"><option value=0>Choose Eatery</option>
-        ${eateries.map(eatery => {return `<option value="${eatery.id}" id="attraction--${eatery.id}">${eatery.businessName}</option>`}).join("")}</select>`
+        ${eateries.map(eatery => {return `<option value="${eatery.id}" id="eatery--${eatery.id}">${eatery.businessName}</option>`}).join("")}</select>`
 }
 
 document.addEventListener(
